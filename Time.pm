968cd6d341b4c49c38ef4734f57b7cee9e241a6b
Chris Williams <chris@bingosnet.co.uk>
2006-08-10 14:50:20 +0000

    Import of BINGOS/POE-Component-Server-Time-1.01 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-Time
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         BINGOS/POE-Component-Server-Time-1.01.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- Time.pm
+++ Time.pm
@@ -19,19 +19,19 @@ use vars qw($VERSION);
 use constant DATAGRAM_MAXLEN => 1024;
 use constant DEFAULT_PORT => 37;
 
-$VERSION = '1.0';
+$VERSION = '1.01';
 
 sub spawn {
-  my ($package) = shift;
+  my $package = shift;
   croak "$package requires an even number of parameters" if @_ & 1;
 
   my %parms = @_;
 
-  $parms{'Alias'} = 'Time-Server' unless ( defined ( $parms{'Alias'} ) and $parms{'Alias'} );
-  $parms{'tcp'} = 1 unless ( defined ( $parms{'tcp'} ) and $parms{'tcp'} == 0 );
-  $parms{'udp'} = 1 unless ( defined ( $parms{'udp'} ) and $parms{'udp'} == 0 );
+  $parms{'Alias'} = 'Time-Server' unless defined $parms{'Alias'} and $parms{'Alias'};
+  $parms{'tcp'} = 1 unless defined $parms{'tcp'} and $parms{'tcp'} == 0;
+  $parms{'udp'} = 1 unless defined $parms{'udp'} and $parms{'udp'} == 0;
 
-  my ($self) = bless( { }, $package );
+  my $self = bless { }, $package;
 
   $self->{CONFIG} = \%parms;
 
@@ -52,7 +52,7 @@ sub accept_new_client {
   my ($kernel,$self,$socket,$peeraddr,$peerport,$wheel_id) = @_[KERNEL,OBJECT,ARG0 .. ARG3];
   $peeraddr = inet_ntoa($peeraddr);
 
-  my ($wheel) = POE::Wheel::ReadWrite->new (
+  my $wheel = POE::Wheel::ReadWrite->new (
         Handle => $socket,
         Filter => POE::Filter::Line->new(),
         InputEvent => 'client_input',
@@ -60,21 +60,20 @@ sub accept_new_client {
 	FlushedEvent => 'client_flushed',
   );
 
-  $self->{Clients}->{ $wheel->ID() }->{Wheel} = $wheel;
-  $self->{Clients}->{ $wheel->ID() }->{peeraddr} = $peeraddr;
-  $self->{Clients}->{ $wheel->ID() }->{peerport} = $peerport;
+  $self->{Clients}->{ $wheel->ID() } = { Wheel => $wheel, peeraddr => $peeraddr, peerport => $peerport };;
   $wheel->put( time );
+  undef;
 }
 
 sub client_input {
-  my ($kernel,$self,$input,$wheel_id) = @_[KERNEL,OBJECT,ARG0,ARG1];
+  undef;
 }
 
 sub client_flushed {
   my ($kernel,$self,$wheel_id) = @_[KERNEL,OBJECT,ARG0];
-
-  delete ( $self->{Clients}->{ $wheel_id }->{Wheel} );
-  delete ( $self->{Clients}->{ $wheel_id } );
+  delete $self->{Clients}->{ $wheel_id }->{Wheel};
+  delete $self->{Clients}->{ $wheel_id };
+  undef;
 }
 
 sub get_datagram {
@@ -86,7 +85,7 @@ sub get_datagram {
   my $output = time();
   send( $socket, $output, 0, $remote_address ) == length( $output )
       or warn "Trouble sending response: $!";
-
+  undef;
 }
 
 1;
@@ -98,9 +97,9 @@ POE::Component::Server::Time - a POE component implementing a RFC 868 Time serve
 
 =head1 SYNOPSIS
 
-use POE::Component::Server::Time;
+ use POE::Component::Server::Time;
 
- my ($self) = POE::Component::Server::Time->spawn( 
+ my $self = POE::Component::Server::Time->spawn( 
 	Alias => 'Time-Server',
 	BindAddress => '127.0.0.1',
 	BindPort => 7777,
@@ -118,7 +117,15 @@ L<POE|POE>. It is a class inherited from L<POE::Component::Server::Echo|POE::Com
 
 =item spawn
 
-Takes a number of optional values: "Alias", the kernel alias that this component is to be blessed with; "BindAddress", the address on the local host to bind to, defaults to L<POE::Wheel::SocketFactory|POE::Wheel::SocketFactory> default; "BindPort", the local port that we wish to listen on for requests, defaults to 37 as per RFC, this will require "root" privs on UN*X; "options", should be a hashref, containing the options for the component's session, see L<POE::Session|POE::Session> for more details on what this should contain.
+Takes a number of optional values: 
+
+  "Alias", the kernel alias that this component is to be blessed with; 
+  "BindAddress", the address on the local host to bind to, defaults to 
+                 POE::Wheel::SocketFactory> default; 
+  "BindPort", the local port that we wish to listen on for requests, 
+              defaults to 37 as per RFC, this will require "root" privs on UN*X; 
+  "options", should be a hashref, containing the options for the component's session, 
+             see POE::Session for more details on what this should contain.
 
 =back
 
@@ -132,10 +139,14 @@ Chris 'BinGOs' Williams, <chris@bingosnet.co.uk>
 
 =head1 SEE ALSO
 
-L<POE|POE>
-L<POE::Session|POE::Session>
-L<POE::Wheel::SocketFactory|POE::Wheel::SocketFactory>
-L<POE::Component::Server::Echo|POE::Component::Server::Echo>
+L<POE>
+
+L<POE::Session>
+
+L<POE::Wheel::SocketFactory>
+
+L<POE::Component::Server::Echo>
+
 L<http://www.faqs.org/rfcs/rfc868.html>
 
 =cut
