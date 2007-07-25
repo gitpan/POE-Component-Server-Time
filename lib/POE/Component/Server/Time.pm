9a5d8557fba45071e992abd9593c7b07e2c203e2
Chris Williams <chris@bingosnet.co.uk>
2007-07-25 11:51:29 +0000

    Import of BINGOS/POE-Component-Server-Time-1.06 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-Time
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         BINGOS/POE-Component-Server-Time-1.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/Server/Time.pm
+++ lib/POE/Component/Server/Time.pm
@@ -19,7 +19,7 @@ use vars qw($VERSION);
 use constant DATAGRAM_MAXLEN => 1024;
 use constant DEFAULT_PORT => 37;
 
-$VERSION = '1.05';
+$VERSION = '1.06';
 
 sub spawn {
   my $package = shift;
@@ -93,7 +93,7 @@ __END__
 
 =head1 NAME
 
-POE::Component::Server::Time - a POE component implementing a RFC 868 Time server.
+POE::Component::Server::Time - A POE component that implements an RFC 868 Time server.
 
 =head1 SYNOPSIS
 
